ce33565cdd9bad057e5a8ba3712b7af24cf5ce28
yashmunjal2 <91688280+yashmunjal2@users.noreply.github.com>
2021-10-11 23:36:43 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                 use:[
                     {
                         loader:"html-loader",
-                        options:{minimize:true}
+                        options:{minimize:false}
                     }
                 ]
             }
